93bf22dc9ca2ec96bfc983c77badf5e6e00d154c
Damien Plenard <damien@plenard.me>
2017-03-16 22:51:52 +0100

    improve code readability

--- src/main.ino
+++ src/main.ino
@@ -1,78 +1,77 @@
 #if defined (HDC_SENSOR)
-#include <Wire.h>
-#include <Adafruit_HDC1000.h>
+  #include <Wire.h>
+  #include <Adafruit_HDC1000.h>
 #elif defined (DHT_SENSOR)
-#include <DHT.h>
+  #include <DHT.h>
 #endif
 #include <Homie.h>
 
-const int INTERVAL = 300;
-
-unsigned long lastTemperatureSent = 0;
-unsigned long lastHumiditySent = 0;
+unsigned long lastTimeSent = 0;
 
 HomieNode temperatureNode("temperature", "temperature");
 HomieNode humidityNode("humidity", "humidity");
 
+#define INTERVAL 300
 #if defined (HDC_SENSOR)
-Adafruit_HDC1000 sensor = Adafruit_HDC1000();
+  #define SDA 2
+  #define SDL 14
+  Adafruit_HDC1000 sensor = Adafruit_HDC1000();
 #elif defined (DHT_SENSOR)
-#define DHTPIN D4
-#define DHTTYPE DHT11
-DHT sensor(DHTPIN, DHTTYPE);
+  #define DHTPIN D4
+  #define DHTTYPE DHT11
+  DHT sensor(DHTPIN, DHTTYPE);
 #endif
 
 void sendTemperature() {
-  if (millis() - lastTemperatureSent >= INTERVAL * 1000UL
-  || lastTemperatureSent == 0) {
     float temperature = sensor.readTemperature();
+
     Serial.print("Temperature: ");
     Serial.print(temperature);
     Serial.println(" °C");
-    if (Homie.setNodeProperty(temperatureNode, "temperature", String(temperature), true)) {
-      lastTemperatureSent = millis();
-    } else {
+
+    if (!Homie.setNodeProperty(temperatureNode, "temperature", String(temperature), true)) {
       Serial.println("Sending temperature failed");
     }
-  }
 }
 
 void sendHumidity() {
-  if (millis() - lastHumiditySent >= INTERVAL * 1000UL
-  || lastHumiditySent == 0) {
-    float humidity = sensor.readHumidity();
-    Serial.print("Humidity: ");
-    Serial.print(humidity);
-    Serial.println("%");
-    if (Homie.setNodeProperty(humidityNode, "humidity", String(humidity), true)) {
-      lastHumiditySent = millis();
-    } else {
-      Serial.println("Sending humidity failed");
-    }
+  float humidity = sensor.readHumidity();
+
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.println("%");
+
+  if (!Homie.setNodeProperty(humidityNode, "humidity", String(humidity), true)) {
+    Serial.println("Sending humidity failed");
   }
 }
 
 void loopHandler() {
-  sendTemperature();
-  sendHumidity();
+  if (millis() - lastTimeSent >= INTERVAL * 1000UL || lastTimeSent == 0) {
+    sendHumidity();
+    sendTemperature();
+    lastTimeSent = millis();
+  }
 }
 
 void setupHandler() {
   Homie.setNodeProperty(temperatureNode, "unit", "c", true);
   Homie.setNodeProperty(humidityNode, "unit", "%", true);
   #if defined (HDC_SENSOR)
-  // Set SDA and SDL ports
-  Wire.begin(2, 14);
+  Wire.begin(SDA, SDL);
   #endif
   sensor.begin();
 }
 
 void setup() {
   Homie.setFirmware("iot-temp", "1.0.0");
+
   Homie.registerNode(temperatureNode);
   Homie.registerNode(humidityNode);
+
   Homie.setSetupFunction(setupHandler);
   Homie.setLoopFunction(loopHandler);
+
   Homie.setup();
 }
 
